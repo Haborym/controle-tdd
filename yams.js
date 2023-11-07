17ea19c21cd759584938fbdd01fa8672cbf8e0fe
Arthur Vadrot <krafteck666@gmail.com>
2023-11-07 14:56:39 +0100

    feat: Added the generating function for the dices

--- yams.js
+++ yams.js
@@ -1,4 +1,11 @@
-function yams() {
+function yams(dices_roll) {
     return true;
 }
+
+function generateDiceRolls() {
+    return [...Array(5)].map(_=>(([...Array(5)].map(_=>Math.ceil(Math.random()*5)+1))))
+}
+
 module.exports = yams;
+
+module.exports = generateDiceRolls;
--- yams.test.js
+++ yams.test.js
@@ -1,4 +1,5 @@
 const yams = require('./yams')
+const dices = require('./yams')
 
 test('return true', () => {
     expect(yams()).toBeTruthy();
